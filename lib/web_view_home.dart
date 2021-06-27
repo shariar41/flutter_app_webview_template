c34ce4ef83ae9cfef80a58a1faa934054fe623e7
shariar41 <shariar41@gmail.com>
2021-06-27 14:21:01 +0600

    reload button added

--- lib/web_view_home.dart
+++ lib/web_view_home.dart
@@ -11,44 +11,95 @@ class WebViewHomeScreen extends StatefulWidget {
 
 class _WebViewHomeScreenState extends State<WebViewHomeScreen> {
   bool hasInternet = false, isChecking = true;
+  TextEditingController controller = TextEditingController();
+  FlutterWebviewPlugin flutterWebviewPlugin = FlutterWebviewPlugin();
+  var url = "https://google.com";
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged wvs) {});
+  }
+
+  searchURL() {
+    setState(() {
+      url = "https://www." + controller.text;
+      flutterWebviewPlugin.reloadUrl(url);
+      controller.text = url;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: check(),
         builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return SafeArea(
-                child: Scaffold(
-                  body: WebviewScaffold(
-                    url: "https://www.google.com",
-                    withZoom: true,
-                    withLocalStorage: true,
-                    // hidden: true,
-                    withJavascript: true,
-                    scrollBar: false,
-                    initialChild: Container(
-                      color: Colors.white,
-                      child: const Center(
-                        child:CircularProgressIndicator(),
+          if (hasInternet ||
+              snapshot.connectionState != ConnectionState.active) {
+            return SafeArea(
+              child: Scaffold(
+                body: WebviewScaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.green,
+                    leading: InkWell(
+                      onTap: (){
+                        setState(() {
+                          flutterWebviewPlugin.reloadUrl(url);
+                        });
+                      },
+                        child: Icon(Icons.refresh)
+                    ),
+                    title: TextField(
+                      controller: controller,
+                      textInputAction: TextInputAction.search,
+                      onSubmitted: (url) => searchURL(),
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          hintText: "Search Here",
+                          hintStyle: TextStyle(color: Colors.white)),
+                    ),
+                    actions: [
+                      IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {
+                            flutterWebviewPlugin.goBack();
+                            controller.text = "";
+                          }),
+                      IconButton(
+                        icon: Icon(Icons.arrow_forward),
+                        onPressed: searchURL,
                       ),
+                    ],
+                  ),
+                  url: "https://www.google.com",
+                  withZoom: true,
+                  withLocalStorage: true,
+                  appCacheEnabled: true,
+                  // hidden: true,
+                  withLocalUrl: true,
+                  withJavascript: true,
+                  scrollBar: true,
+                  initialChild: Container(
+                    color: Colors.white,
+                    child: const Center(
+                      child: CircularProgressIndicator(),
                     ),
                   ),
                 ),
-              );
-            default:{
-              return Center(
-                child: Container(
-                  color: Colors.white,
-                  //padding: const EdgeInsets.only(top: 50),
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
+              ),
+            );
+          } else {
+            return Center(
+              child: Container(
+                color: Colors.white,
+                //padding: const EdgeInsets.only(top: 50),
+                child: CircularProgressIndicator(),
+              ),
+            );
           }
-        }
-    );
+        });
   }
+
   check() async {
     var connectivityResult = await Connectivity().checkConnectivity();
     if (connectivityResult == ConnectivityResult.mobile) {
